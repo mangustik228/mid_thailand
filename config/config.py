4d186a257b3adfd4d0066707216b36d5e2a03144
mangustik228 <bacek.mangust@gmail.com>
2023-07-14 11:06:13 +0700

    send logs

--- config/config.py
+++ config/config.py
@@ -10,15 +10,19 @@ dotenv.load_dotenv()
 config_ini = ConfigParser()
 config_ini.read('config/config.ini')
 
+
 class Bot(BaseSettings):
     token: str = os.getenv('bot_token')
-    my_id: int = os.getenv('my_id')
+    my_id: str = os.getenv('my_id')
+
 
 class Captcha(BaseSettings):
     token: str = os.getenv('captcha_token')
 
+
 class Parsing(BaseSettings):
     retries: int = config_ini.getint("PARSING", "retries")
+
 
 class Logs(BaseSettings):
     sink: str = config_ini.get('LOGS', 'path')
@@ -26,6 +30,7 @@ class Logs(BaseSettings):
     rotation: str = config_ini.get('LOGS', 'rotation')
     format: str = config_ini.get('LOGS', 'format')
     compression: str = config_ini.get('LOGS', 'compression')
+
 
 class Config(BaseSettings):
     debug: bool = config_ini.getboolean('DEFAULT', 'debug')
@@ -35,5 +40,5 @@ class Config(BaseSettings):
     bot: Bot = Bot()
     parsing: Parsing = Parsing()
 
-config = Config()
 
+config = Config()
--- config/config_log.py
+++ config/config_log.py
@@ -3,21 +3,21 @@ from loguru import logger
 from telebot import TeleBot
 
 
-
 logger.add(
-    config.logs.sink, 
+    config.logs.sink,
     level=config.logs.level,
     rotation=config.logs.rotation,
     format=config.logs.format)
 
 
-def send_message_tg(message:str):
+def send_message_tg(message: str):
     bot = TeleBot(config.bot.token)
-    bot.send_message(config.bot.my_id, message[:500])
+    for id in config.bot.my_id.split(','):
+        bot.send_message(int(id), message[:500])
+
 
 if config.alert:
     logger.level('ALERT', no=39, color="<green>")
     logger.add(send_message_tg, level="ALERT", format='{message}')
 
 logger.debug(f'Проект настроен')
-
