f92008fda6dbb1823f79ea953633b22fd7b7a088
mangustik228 <bacek.mangust@gmail.com>
2023-07-14 11:09:05 +0700

    add headless

--- pars/parser.py
+++ pars/parser.py
@@ -13,7 +13,7 @@ class Parser:
 
     def parse_condition(self):
         with sync_playwright() as pw:
-            browser = pw.chromium.launch(headless=False)
+            browser = pw.chromium.launch(headless=True)
             context = browser.new_context()
             logger.debug(f'Объект браузера создан')
             page = context.new_page()
